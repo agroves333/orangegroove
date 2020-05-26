b7fc8f0b2231db83827c9777f32eb8dcaaf32b57
Adam Groves <agroves333@gmail.com>
2020-05-25 22:04:56 -0400

    center header

--- src/templates/index-page.js
+++ src/templates/index-page.js
@@ -97,6 +97,7 @@ export const IndexPageTemplate = ({
                 color: "white",
                 lineHeight: "1",
                 padding: "0.25em",
+                textAlign: 'center'
               }}
             >
               {title}
